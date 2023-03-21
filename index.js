6d46fe3aa5d80d87a42c770dcaedaa69739214d4
Yinson Tso <yinson.tso@gmail.com>
2023-03-21 18:18:40 -0400

    array-find

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+superbowlWin = (record) => {   
+    
+    for (let result in record){
+        if (record.find(record => record.result === 'W')){
+            let win = record.find(record => record.result === 'W').year
+            return win
+        }
+    }
+    return undefined
+}
